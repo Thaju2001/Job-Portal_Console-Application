b68421e98acb62ddb6819307db9cc4061387c7d1
Trainee-aspire <sng19cs025@sngce.ac.in>
2025-01-10 17:28:09 +0530

    first commit

--- obj/Debug/net9.0/ExchEmp.AssemblyInfo.cs
+++ obj/Debug/net9.0/ExchEmp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExchEmp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b518ea6ea00ecd952b6860af4cb6791554cf093c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b03c3efaecc405e237cea95dd2a8b4d280502341")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExchEmp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExchEmp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
